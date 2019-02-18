1ea730fed70303e98fb565ccc2cc6e2fbccf0310
mcadcliff <44850474+mcadcliff@users.noreply.github.com>
2019-02-17 22:30:23 -0600

    update script.js

--- assignment4/script.js
+++ assignment4/script.js
@@ -1,45 +1,46 @@
-var theirFinestHour = {name: “Winston Churchill”, 
-                       year: “1940”, 
-                       bce: “false”,}
+var churchill = {name: “Winston Churchill”, 
+                       year: 1940, 
+                       bce: false};
 
-var quitIndia = {name: “Ghandi”,
-                 year: “1942”, 
-                 bce: “false”,} 
+var ghandi = {name: “Ghandi”,
+                 year: 1942, 
+                 bce: false}; 
 
-var theThirdPhilippic = {name: “Demosthenes”,
-       	                year: “342”, 
-                        bce: “true”,}
+var demosthenes = {name: “Demosthenes”,
+       	                year: 342, 
+                        bce: true};
 //object of speeches//
 
 
-speeches = [theirFinestHour, quitIndia, theThirdPhilippic];
+var speeches = [churchill, ghandi, demosthenes];
 //array of speeches’ objects//
 
  
-var difference = (quitIndia.year - theirFinestHour.year);
-console.log (“Ghandi and Churchill’s speeches are “ + difference + “ years apart.”);
+var difference = (ghandi.year - churchill.year);
 // difference between speeches’ years//
+
+window.onload = function()
+{console.log(“Ghandi and Churchill’s speeches are “ + difference + “ years apart.”);};
 
 
 document.getElementById('BtnDonate').addEventListener('click', function(){ 
-});
-console.log ("There are " + speeches.length + "speeches on this page.")
+console.log ("There are " + speeches.length + "speeches on this page.")});
  //Code in here executes when the user clicks the "Donate" button.
 
 
 document.getElementById('BtnChurchill').addEventListener('click', function(){
-});
-console.log ("This speech was written by " + theirFinestHour + " in" + year + ".")("It is " + bce + " that this year is B.C.E.")
+console.log ("This speech was written by " + churchill.name + " in" + churchill.year + ".")
+("It is " + churchill.bce + " that this year is B.C.E.")});
  //Code in here executes when the user clicks the "Churchill" button.
 
 
 document.getElementById('BtnGhandi').addEventListener('click', function(){
-});
-console.log ("This speech was written by " + quitIndia + " in" + year + ".")("It is " + bce + " that this year is B.C.E.")
+console.log ("This speech was written by " + ghandi.name + " in" + ghandi.year + ".")
+("It is " + ghandi.bce + " that this year is B.C.E.")});
   //Code in here executes when the user clicks the "Ghandi" button.
 
 
 document.getElementById('BtnDemosthenes').addEventListener('click', function(){
-});
-console.log ("This speech was written by " + theThirdPhilippic + " in" + year + ".")("It is " + bce + " that this year is B.C.E.")
+console.log ("This speech was written by " + demosthenes.name + " in" + demosthenes.year + ".")
+("It is " + demosthenes.bce + " that this year is B.C.E.")});
   //Code in here executes when the user clicks the "Demosthenes" button.
